b25f92bb22d7c310bce76842de11aadd2755499c
akshaya <akshayasunny@gmail.com>
2015-11-18 21:45:16 +0530

    TestShare added image share

--- TestShare/app/src/main/java/com/example/root/testshare/MainActivity.java
+++ TestShare/app/src/main/java/com/example/root/testshare/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.root.testshare;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private ShareActionProvider mShareActionProvider;
+    Intent imageIntent;
+    Intent sendIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +40,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_main, menu);
         MenuItem item = menu.findItem(R.id.menu_item_share);
         mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
 
+
         //defining intent, and attaching it to the menu item
-        Intent sendIntent = new Intent();
+        sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-        sendIntent.setType("text/plain");
-        //startActivity(sendIntent);
-        mShareActionProvider.setShareIntent(sendIntent);
+
+
+        Boolean image = true;//false for text
+        if(!image) {
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+            sendIntent.setType("text/plain");
+            //startActivity(sendIntent);
+            mShareActionProvider.setShareIntent(sendIntent);
+        }
+        else{
+            imageIntent = new Intent();
+            imageIntent.setType("image/*");
+            imageIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(imageIntent, "Select Picture"), 1);
+        }
         return true;
     }
 
-    /*private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Uri selectedImageUri = data.getData();
+                sendIntent.putExtra(Intent.EXTRA_STREAM, selectedImageUri);
+                sendIntent.setType("image/*");
+                mShareActionProvider.setShareIntent(sendIntent);
+            }
         }
-    }*/
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
